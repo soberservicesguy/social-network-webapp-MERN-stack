ce68f067ae791dfcda2d01a5faee1d079704d463
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-02-12 13:53:53 +0500

    worked more

--- frontend/src/components/socialposts/create_social_post.js
+++ frontend/src/components/socialposts/create_social_post.js
@@ -24,84 +24,6 @@ import withResponsiveness from "../../responsiveness_hook";
 
 
 
-// const styles = theme => ({
-// 	roundTextInputContainer:{
-// 		width:'80%', 
-// 		margin:'auto',
-// 	},
-// 	roundTextInput:{
-// 		outline:'none', 
-// 		width:'100%', 
-// 		height:50, 
-// 		paddingLeft:20,
-// 		paddingRight:100, 
-// 		color:'black', 
-// 		borderRadius:30,
-// 		borderWidth:1, 
-// 		borderStyle:'solid',
-// 		borderColor:'black', 
-// 		backgroundColor: '#eee'
-// 	},
-
-
-
-
-
-// 	roundButtonInsideTextInputContainer:{
-// 		width: '20%',
-// 		// width: 100,
-// 		height: 40,
-// 		backgroundColor: '#000000',
-// 		borderRadius: props => props.tracked_height2 / 2,
-// 		borderWidth: 1, 
-// 		borderStyle: 'solid',
-// 		borderColor: 'black', 
-
-// 		position: 'relative',
-// 		bottom: props => (props.tracked_height2 + 2) + (props.tracked_height1 + 2 - props.tracked_height2 - 2)/2, // self_height_including_border_thickness + difference in heights of both / 2
-// 		left: props => props.tracked_width1 + 2 - props.tracked_width2 - 10, // tracked_width - self_width - some_gap 
-// 	},
-
-// 	// root: {
-// 	// 	height: 100,
-// 	// 	backgroundColor: '#000000',
-// 	// 	color: props => (props.cool) ? 'red' : 'black',
-// 	// 	[theme.breakpoints.up('sm')]:{
-// 	// 		// paddingLeft:200
-// 	// 	},
-// 	// },
-// 	// buttonWithoutBG:{
-// 	// 	marginTop:50,
-// 	// 	marginBottom:50,
-// 	// },
-// 	// innerText:{
-
-// 	// },
-// 	// textinputContainer:{
-// 	// 	// marginTop: windowHeight * 0.05, // or 30  gap
-// 	// 	// height: windowHeight * 0.1, // or 100
-// 	// 	// width: '100%',
-// 	// 	width:'80%',
-// 	// 	justifyContent: 'center', // vertically centered
-// 	// 	alignSelf: 'center', // horizontally centered
-// 	// 	backgroundColor: utils.lightGreen,
-// 	// },
-// 	// textinput:{
-// 	// 	marginTop:20,
-// 	// 	textAlign:'left',
-// 	// 	borderWidth:1,
-// 	// 	borderColor:(utils.lightGrey),
-// 	// 	borderStyle:'solid',
-// 	// 	paddingLeft:20,
-// 	// 	paddingTop:15,
-// 	// 	paddingBottom:15,
-// 	// 	fontSize:18,
-// 	// },
-// 	// outerContainer: {
-// 	// },
-// 	// bigBlue: {
-// 	// },
-// });
 
 
 class CreateSocialPost extends Component {
@@ -153,13 +75,35 @@ class CreateSocialPost extends Component {
 
 		const styles = {
 			outerContainer:{
+				backgroundColor: 'white',
+			},
 
+			avatarAndCreatePostContainer:{
+				width:'95%',
+				margin:'auto',
+				display:'flex',
+				flexDirection:'row',
+				height:100,
+				// backgroundColor: '#000000'
+				justifyContent: 'center',
+				alignItems:'center',
+			},
+			createPostContainer:{
+				flex: 5,
+				// backgroundColor: '#000000',
+				paddingTop:10,
+			},
+			avatarContainer:{
+				flex: 1,
 			},
 
 		// round text input
 			roundTextInputContainer:{
-				width:'80%', 
+				width:'95%', 
+				height:50,
 				margin:'auto',
+				// marginBottom:0,
+				// backgroundColor: '#000000',
 			},
 			roundTextInput:{
 				outline:'none', 
@@ -171,8 +115,8 @@ class CreateSocialPost extends Component {
 				borderRadius:30,
 				borderWidth:1, 
 				borderStyle:'solid',
-				borderColor:'black', 
-				backgroundColor: '#eee'
+				borderColor:'#eee', 
+				backgroundColor: '#eee',
 			},
 
 		// roundButtonInsideTextInput
@@ -180,18 +124,42 @@ class CreateSocialPost extends Component {
 				width: '20%',
 				// width: 100,
 				height: 40,
-				backgroundColor: '#000000',
+				backgroundColor: utils.maroonColor,
 				borderRadius: this.state.tracked_height2 / 2,
 				borderWidth: 1, 
 				borderStyle: 'solid',
-				borderColor: 'black', 
+				borderColor: utils.maroonColor, 
 
 				position: 'relative',
 				bottom: (this.state.tracked_height2 + 2) + (this.state.tracked_height1 + 2 - this.state.tracked_height2 - 2)/2, // self_height_including_border_thickness + difference in heights of both / 2
 				left: this.state.tracked_width1 + 2 - this.state.tracked_width2 - 10, // tracked_width - self_width - some_gap 
 			},
+			roundButtonInsideTextInput:{
+				width:'100%',
+				height:'100%',
+				border:'none',
+				background: 'none',
+				outline:'none',
+				color:'white',
+				fontWeight:'bold',
+			},
+
+			uploadImageAndVideoContainer:{
+				margin:'auto',
+				marginTop:5,
+				width:'75%',
+				display:'flex',
+				flexDirection:'row',
+				justifyContent: 'space-between' 
+			},
+			uploadImageAndVideoButton:{
+				fontWeight:'bold',
+				color:utils.maroonColor
+			}
 
 		}
+
+		// var base64Image = "data:image/jpeg;base64," + this.props.current_image
 
 		if ( this.state.redirectToRoute !== false ){
 
@@ -202,117 +170,136 @@ class CreateSocialPost extends Component {
 			return <Redirect to = {{ pathname: "/Individual-SocialPost" }} />
 
 		} else {
-
 			return (
 			// e.g a social post, textinput which lets user to enter text, takes persons id as assigned object
 				<div style={styles.outerContainer}>
 
-				{/* round text input */}
-					<div style={styles.roundTextInputContainer}>
-						<form>
-							<input 
-								ref={ (divElement) => { this.divElement1 = divElement } }
-								placeholder="What's in your mind" 
-								type="text" 
-								name="post_text"
-								multiline={true}
-								onChange={ (event) => this.setState( prev => ({...prev, post_text: event.target.value})) }
-								style={styles.roundTextInput} 
+					<div style={styles.avatarAndCreatePostContainer}>
+						<div styles={styles.avatarContainer}>
+							<img 
+								alt="" 
+								// src={base64Image}
+								src={utils.image}
+								// src={require("../../images/samosa.jpeg").default}
+								style={{
+									width:80,
+									height:80,
+									resizeMode: "stretch",
+									borderRadius: 80/2,
+								}}
 							/>
-						</form>
-					{/* round button inside round text input */}
-						<div 
-							ref={ (divElement) => { this.divElement2 = divElement } }
-							style={styles.roundButtonInsideTextInputContainer}
-						>
-							<p style={{
-								color:'white',
-								textAlign:'center',
-								margin:'auto',
-								paddingTop:10,
-								paddingBottom:10,
-							}}>
-								Create Post
-							</p>
 						</div>
-					</div>
 
-				{/*image upload*/}
-					<div>
-						<label htmlFor="myImageInput">
-							{/* below div will act as myInput button*/}
-							<div style={{width:20, height:20, backgroundColor: '#000000'}}>
+						<div style={styles.createPostContainer}>
+							{/* round text input */}
+							<div style={styles.roundTextInputContainer}>
+								<form>
+									<input 
+										ref={ (divElement) => { this.divElement1 = divElement } }
+										placeholder="What's in your mind" 
+										type="text" 
+										name="post_text"
+										multiline={true}
+										onChange={ (event) => this.setState( prev => ({...prev, post_text: event.target.value})) }
+										style={styles.roundTextInput} 
+									/>
+								</form>
+
+								{/* round button inside round text input */}
+								<div 
+									ref={ (divElement) => { this.divElement2 = divElement } }
+									style={styles.roundButtonInsideTextInputContainer}
+								>
+									<button 
+										style={styles.roundButtonInsideTextInput}
+										onClick={ () => {
+
+											let setResponseInCurrentSocialPost = (arg) => this.props.set_current_socialpost(arg)
+											let redirectToNewSocialPost = () => this.setState(prev => ({...prev, redirectToRoute: (prev.redirectToRoute === false) ? true : false }))	
+
+											const formData = new FormData()
+
+											formData.append('post_text', this.state.post_text)
+											if(this.state.image_upload !== ''){
+												formData.append('image_upload', this.state.image_upload, this.state.image_upload.name)
+											}
+											if(this.state.video_upload !== ''){
+												formData.append('video_upload', this.state.video_upload, this.state.video_upload.name)
+											}
+
+											axios.post(utils.baseUrl + '/socialposts/create-socialpost-with-user', formData)
+											.then(function (response) {
+												console.log(response.data) // current socialpost screen data
+												
+												// set to current parent object
+												setResponseInCurrentSocialPost(response.data)
+
+												// change route to current_socialpost
+												redirectToNewSocialPost()
+
+											})
+											.catch(function (error) {
+												console.log(error)
+											});						
+
+										}}
+									>
+										Create Post
+									</button>
+								</div>
+
+
 							</div>
-						</label>
-						<input
-							id="myImageInput"
-							style={{display:'none'}}
-							type={"file"}
-							onChange={(event) => {
-								// console logging selected file from menu
-								// setState method with event.target.files[0] as argument
-								this.setState(prev => ({...prev, image_upload: event.target.files[0]}))
-								console.log( event.target.files[0] ) // gives first file
-							}}
-						/>
-					</div>
 
-				{/*video upload*/}
-					<div>
-						<label htmlFor="myVideoInput">
-							{/* below div will act as myInput button*/}
-							<div style={{width:20, height:20, backgroundColor: '#000000'}}>
-							</div>
-						</label>
-						<input
-							id="myVideoInput"
-							style={{display:'none'}}
-							type={"file"}
-							onChange={(event) => {
-								// console logging selected file from menu
-								console.log( event.target.files[0] ) // gives first file
-								// setState method with event.target.files[0] as argument
-								this.setState(prev => ({...prev, video_upload: event.target.files[0]}))
-							}}
-
-						/>
-					</div>
-
-
-
-					<button style={styles.buttonWithoutBG}
-						onClick={ () => {
-
-							let setResponseInCurrentSocialPost = (arg) => this.props.set_current_socialpost(arg)
-							let redirectToNewSocialPost = () => this.setState(prev => ({...prev, redirectToRoute: (prev.redirectToRoute === false) ? true : false }))	
-
-							const formData = new FormData()
-
-							formData.append('post_text', this.state.post_text)
-							formData.append('image_upload', this.state.image_upload, this.state.image_upload.name)
-							formData.append('video_upload', this.state.video_upload, this.state.video_upload.name)
-
-							axios.post(utils.baseUrl + '/socialposts/create-socialpost-with-user', formData)
-							.then(function (response) {
-								console.log(response.data) // current socialpost screen data
+							<div style={styles.uploadImageAndVideoContainer}>
 								
-								// set to current parent object
-								setResponseInCurrentSocialPost(response.data)
+								{/*image upload*/}
+								<div>
+									<label htmlFor="myImageInput">
+										{/* below div will act as myInput button*/}
+										<div style={styles.uploadImageAndVideoButton}>
+											Upload Image
+										</div>
+									</label>
+									<input
+										id="myImageInput"
+										style={{display:'none'}}
+										type={"file"}
+										onChange={(event) => {
+											// console logging selected file from menu
+											// setState method with event.target.files[0] as argument
+											this.setState(prev => ({...prev, image_upload: event.target.files[0]}))
+											console.log( event.target.files[0] ) // gives first file
+										}}
+									/>
+								</div>
 
-								// change route to current_socialpost
-								redirectToNewSocialPost()
+								{/*video upload*/}
+								<div>
+									<label htmlFor="myVideoInput">
+										{/* below div will act as myInput button*/}
+										<div style={styles.uploadImageAndVideoButton}>
+											Upload Video
+										</div>
+									</label>
+									<input
+										id="myVideoInput"
+										style={{display:'none'}}
+										type={"file"}
+										onChange={(event) => {
+											// console logging selected file from menu
+											console.log( event.target.files[0] ) // gives first file
+											// setState method with event.target.files[0] as argument
+											this.setState(prev => ({...prev, video_upload: event.target.files[0]}))
+										}}
 
-							})
-							.catch(function (error) {
-								console.log(error)
-							});						
+									/>
+								</div>
+							</div>
 
-						}}
-					>
-						<p style={styles.innerText}>
-							Press To Create SocialPost
-						</p>
-					</button>
+						</div>
+
+					</div>
 				</div>
 			);
 		}			
--- frontend/src/utilities.js
+++ frontend/src/utilities.js
@@ -2,6 +2,12 @@
 const utils = {
     baseUrl:'https://00a1d01a58f3.ngrok.io',
     productsEndpoint:'',
+
+// THIS IS DIFFERENT FROM NATIVE, IN NATIVE NO .default needed
+    image:require("./images/samosa.jpeg").default,
+
+    maroonColor:'#dc4734',
+
 }
 
 export default utils
