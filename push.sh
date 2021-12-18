b0694d891561f3a04e89d7a0d65af38fea1ee681
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-12-18 13:20:02 +0500

    worked more

--- push.sh
+++ push.sh
@@ -3,9 +3,9 @@
 
 appName='appointment'
 
+# Set docker image location
 dockerLocation='Local'
 # dockerLocation='DockerRegistery'
-
 
 # almost same in all
 baseURL_for_App='http://localhost:3001'
@@ -18,7 +18,7 @@ aws_s3_accessKeyId='AKIAW2YVB4HUWASTJCER'
 aws_s3_secretAccessKey='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'
 aws_s3_bucket='portfolio-apps-mern-native'
 
-# FIXED
+# FIXED FOR ALL
 frontendURL_for_App='http://localhost:3000'
 frontendURL_for_Containerized_Version=$baseURL_for_Containerized_Version
 frontendURL_for_Kubernetes_Version=$baseURL_for_Kubernetes_Version
@@ -31,10 +31,10 @@ docker_images_path='./Containers_Version/image_sources'
 kubernetes_containers_path='./Kubernetes_Version/container_sources'
 kubernetes_path='./Kubernetes_Version'
 
-# updateBackendRoutesIntoContainersAndKubernetesFolders $app_backend_file_path $docker_images_path $kubernetes_containers_path
-# generateVersionAppBuildAndCopyToBackend $baseURL_for_App $utilities_file_path
-# generateVersionContainerBuildAndCopyToBackend $baseURL_for_Containerized_Version $utilities_file_path  
-# generateVersionKubernetesBuildAndCopyToBackend $baseURL_for_Kubernetes_Version $utilities_file_path
-# generateDockerImages $kubernetes_containers_path $kubernetes_path
-# deployApp $baseURL_for_Heroku $utilities_file_path $aws_s3_accessKeyId $aws_s3_secretAccessKey $aws_s3_bucket
+updateBackendRoutesIntoContainersAndKubernetesFolders $app_backend_file_path $docker_images_path $kubernetes_containers_path
+generateVersionAppBuildAndCopyToBackend $baseURL_for_App $utilities_file_path
+generateVersionContainerBuildAndCopyToBackend $baseURL_for_Containerized_Version $utilities_file_path  
+generateVersionKubernetesBuildAndCopyToBackend $baseURL_for_Kubernetes_Version $utilities_file_path
+generateDockerImages $kubernetes_containers_path $kubernetes_path $dockerLocation $appName $kubernetes_containers_path $kubernetes_path
+deployApp $baseURL_for_Heroku $utilities_file_path $aws_s3_accessKeyId $aws_s3_secretAccessKey $aws_s3_bucket
 createNewGithubPR $baseURL_for_App $app_backend_file_path $docker_images_path $kubernetes_containers_path $baseURL_for_Containerized_Version $baseURL_for_Kubernetes_Version $app_backend_env_file_path
