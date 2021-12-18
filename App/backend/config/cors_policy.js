a4202b08a84f032f8ffd9d74c5e583335f5cd499
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-12-18 13:01:54 +0500

    worked more

--- App/backend/config/cors_policy.js
+++
@@ -1,9 +0,0 @@
-
-const cors = require("cors");
-
-let cors_policy = cors({
-	origin: "https://appointment-mern-web.herokuapp.com", // restrict calls to those this address
-	methods: ['GET', 'POST'] // only allow GET, POST requests
-})
-
-module.exports = cors_policy
