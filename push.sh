e2ded056a2d104b6a6ea6d36e6e89b3a4ec5edc7
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-12-19 11:13:54 +0500

    worked more

--- push.sh
+++ push.sh
@@ -9,6 +9,7 @@ dockerLocation='Local'
 
 # change for each project
 baseURL_for_Heroku='https://social-mern-stack.herokuapp.com'
+mongodb_prod_string="mongodb+srv://DBuser:AnimatioN1010@simpleclusterforandroid.enzb4.mongodb.net/Appointment_Web_App_DB?retryWrites=true&w=majority"
 aws_s3_accessKeyId='AKIAW2YVB4HUWASTJCER'
 aws_s3_secretAccessKey='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'
 aws_s3_bucket='portfolio-apps-mern-native'
@@ -30,12 +31,12 @@ app_backend_env_file_path='./App/backend/.env'
 docker_images_path='./Containers_Version/image_sources'
 kubernetes_containers_path='./Kubernetes_Version/container_sources'
 kubernetes_path='./Kubernetes_Version'
-
+production_db_mode='production'
 
 updateBackendRoutesIntoContainersAndKubernetesFolders $app_backend_file_path $docker_images_path $kubernetes_containers_path
 generateVersionAppBuildAndCopyToBackend $baseURL_for_App $utilities_file_path
 generateVersionContainerBuildAndCopyToBackend $baseURL_for_Containerized_Version $utilities_file_path  
 generateVersionKubernetesBuildAndCopyToBackend $baseURL_for_Kubernetes_Version $utilities_file_path
 generateDockerImages $kubernetes_containers_path $kubernetes_path $dockerLocation $appName $kubernetes_containers_path $kubernetes_path
-deployApp $baseURL_for_Heroku $app_backend_file_path $docker_images_path $kubernetes_containers_path $baseURL_for_Containerized_Version $baseURL_for_Kubernetes_Version $utilities_file_path $aws_s3_accessKeyId $aws_s3_secretAccessKey $aws_s3_bucket $app_backend_env_file_path
+deployApp $baseURL_for_Heroku $app_backend_file_path $docker_images_path $kubernetes_containers_path $baseURL_for_Containerized_Version $baseURL_for_Kubernetes_Version $utilities_file_path $aws_s3_accessKeyId $aws_s3_secretAccessKey $aws_s3_bucket $app_backend_env_file_path $mongodb_prod_string $production_db_mode
 createNewGithubPR $baseURL_for_App $app_backend_file_path $docker_images_path $kubernetes_containers_path $baseURL_for_Containerized_Version $baseURL_for_Kubernetes_Version $app_backend_env_file_path
